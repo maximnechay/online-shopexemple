978fc6b241b271aa5da63657a3fec97ba5af3f4b
Maksym <maximnechay@gmail.com>
2025-12-10 17:19:30 +0100

    feat: remove obsolete Sentry configuration file for improved clarity

--- sentry.client.config.ts
+++
@@ -1,5 +0,0 @@
-// This file is kept for backward compatibility but Sentry is now initialized in instrumentation-client.ts
-// to avoid duplicate initialization warnings.
-// See: https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-export { };
